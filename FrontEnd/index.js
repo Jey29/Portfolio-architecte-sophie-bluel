dc35f515751628f8177a0b7c9fb713920f86e98e
Jeremy Boloh <boloh.jeremy@gmail.com>
2023-02-24 10:07:57 +0100

    commit modal1 avancé

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,58 +1,100 @@
 let projets;
 
+// Récupération du projet depuis le fichier JSON
 fetch("http://localhost:5678/api/works")
   .then((response) => response.json())
   .then((data) => {
     projets = data;
-    genererProjetsGallery(projets);
-    genererProjetsGalleryModal(projets);
+    genererProjets(projets);
+    genererModal(projets);
   })
   .catch((error) => console.error(error));
 
-function genererProjetsGallery(projets) {
+// Fonction qui génère la galerie de projets
+function genererProjets(projets) {
+  // Récupération de l'élément du DOM qui accueillera la galerie
   const sectionGallery = document.querySelector(".gallery");
   for (let i = 0; i < projets.length; i++) {
+    const article = projets[i];
+    // Création d’une balise dédiée à un projet
     const projetElement = document.createElement("figure");
+    // On crée l’élément img.
     const imageElement = document.createElement("img");
     imageElement.src = projets[i].imageUrl;
     imageElement.crossOrigin = "anonymous";
-
+    // On créé l'élément title.
     const titleElement = document.createElement("figcaption");
-    titleElement.innerText = projets[i].title;
+    titleElement.innerText = article.title;
 
     projetElement.appendChild(imageElement);
     projetElement.appendChild(titleElement);
 
+    // On rattache la balise projetElement à la sectionGallery
     sectionGallery.appendChild(projetElement);
   }
 }
 
-function genererProjetsGalleryModal(projets) {
+// Fonction qui génère la modale
+function genererModal(projets) {
+  // Récupération de l'élément du DOM qui accueillera la modale
   const sectionModal = document.querySelector(".gallery-modal");
   for (let i = 0; i < projets.length; i++) {
+    // Création d’une balise dédiée à un projet
     const projetElement = document.createElement("figure");
+    // On crée l’élément img.
     const imageElement = document.createElement("img");
     imageElement.src = projets[i].imageUrl;
     imageElement.crossOrigin = "anonymous";
+    // On créé l'élément du bouton poubelle
+    const deleteButton = document.createElement("button");
+    const deleteIcon = document.createElement("i");
+    deleteIcon.classList.add("fa-solid", "fa-trash-can");
+    deleteButton.appendChild(deleteIcon);
+    deleteButton.addEventListener("click", () => {
+      supprimerProjet(projets[i].id);
+      sectionModal.removeChild(projetElement);
+      const projetGallery = document.getElementById(`gallery-${projets[i].id}`);
+      projetGallery.remove();
+    });
 
     projetElement.appendChild(imageElement);
+    projetElement.appendChild(deleteButton);
 
+    // On rattache la balise projetElement à la sectionModal
     sectionModal.appendChild(projetElement);
   }
 }
 
-function supprimerListe() {
-  document.querySelector(".gallery").innerHTML = "";
-  document.querySelector(".gallery-modal").innerHTML = "";
+// Fonction pour supprimer un projet
+function supprimerProjet(id) {
+  fetch(`https://mon-api.com/projets/${id}`, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erreur lors de la suppression du projet");
+      }
+      // Supprimer le projet de la modale
+      const projetElement = document.getElementById(`projet-${id}`);
+      projetElement.remove();
+      // Supprimer le projet de la gallery
+      const projetGallery = document.getElementById(`gallery-${id}`);
+      projetGallery.remove();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
-fetch("http://localhost:5678/api/works")
-  .then((response) => response.json())
-  .then((data) => {
-    genererProjets(data);
-  })
-  .catch((error) => console.error(error));
+// Fonction pour supprimer la liste des projets dans la galerie
+function supprimerListe() {
+  document.querySelector(".gallery").innerHTML = "";
+}
 
+// Fonction pour supprimer la liste des projets dans la modale
+function supprimerListeModal() {
+  document.querySelector(".gallery-modal").innerHTML = "";
+}
 //////////////////////////les filtres//////////////////////////
 
 const divFilters = document.querySelector("#filters");
@@ -74,8 +116,7 @@ fetch("http://localhost:5678/api/categories")
   })
   .then((categories) => {
     categories.forEach((category) => {
-      const bouton = document.createElement("bouton");
-      //donner le style aux boutons créés
+      const bouton = document.createElement("button");
       bouton.classList.add("bouton-style");
       bouton.textContent = category.name;
 
--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -7,20 +7,20 @@ const modifyBtn = document.getElementById("modify");
 // Récupération du bouton de fermeture de la modal
 const closeModalBtn = document.querySelector(".js-modal-close");
 
-// Fonction pour ouvrir la modal
-const openModal = () => {
-  modal.style.display = null;
-  modal.removeAttribute("aria-hidden");
-  modal.setAttribute("aria-modal", "true");
+// Fonction générique pour ouvrir une modal
+const openModal = (modalEl) => {
+  modalEl.style.display = null;
+  modalEl.removeAttribute("aria-hidden");
+  modalEl.setAttribute("aria-modal", "true");
   closeModalBtn.addEventListener("click", closeModal);
   window.addEventListener("keydown", onKeyDown);
 };
 
-// Fonction pour fermer la modal
-const closeModal = () => {
-  modal.style.display = "none";
-  modal.setAttribute("aria-hidden", "true");
-  modal.removeAttribute("aria-modal");
+// Fonction générique pour fermer une modal
+const closeModal = (modalEl) => {
+  modalEl.style.display = "none";
+  modalEl.setAttribute("aria-hidden", "true");
+  modalEl.removeAttribute("aria-modal");
   closeModalBtn.removeEventListener("click", closeModal);
   window.removeEventListener("keydown", onKeyDown);
   window.removeEventListener("click", onClickOutside);
@@ -29,40 +29,38 @@ const closeModal = () => {
 // Fonction pour gérer l'événement de touche appuyée
 const onKeyDown = (event) => {
   if (event.key === "Escape") {
-    closeModal();
+    closeModal(modal);
+    closeModal(newModal);
   }
 };
 
 // Fonction pour gérer l'événement de clic à l'extérieur de la modal
 const onClickOutside = (event) => {
   if (!modal.contains(event.target)) {
-    closeModal();
+    closeModal(modal);
+    closeModal(newModal);
   }
 };
-console.log(onClickOutside);
+
 // Ajout d'un écouteur d'événement de clic sur le bouton "Modifier"
-modifyBtn.addEventListener("click", openModal);
+modifyBtn.addEventListener("click", () => {
+  openModal(modal);
+});
 
-// let projets;
-// // Fonction qui génère toute la page web
-// function genererProjets(projets) {
-//   // Récupération de l'élément du DOM qui accueillera la gallery
-//   const sectionGallery = document.querySelector(".gallery");
-//   for (let i = 0; i < projets.length; i++) {
-//     const article = projets[i];
-//     // Création d’une balise dédiée à un projet
-//     const projetElement = document.createElement("figure");
-//     // On crée l’élément img.
-//     const imageElement = document.createElement("img");
-//     imageElement.src = projets[i].imageUrl;
-//     imageElement.crossOrigin = "anonymous";
+// Récupération du bouton d'ajout et de la page modale actuelle
+const addButton = document.querySelector(".bouton-ajout");
 
-//     projetElement.appendChild(imageElement);
+// Récupération de la nouvelle page modale
+const newModal = document.querySelector("#modal2");
 
-//     // On rattache la balise article au body
-//     sectionGallery.appendChild(projetElement);
-//   }
-// }
-// function supprimerListe() {
-//   document.querySelector(".gallery").innerHTML = "";
-// }
+// Fonction qui sera appelée lorsque le bouton sera cliqué
+function switchModal() {
+  // Masquer la page modale actuelle
+  closeModal(modal);
+
+  // Afficher la nouvelle page modale
+  openModal(newModal);
+}
+
+// Ajout de l'événement de clic au bouton
+addButton.addEventListener("click", switchModal);
